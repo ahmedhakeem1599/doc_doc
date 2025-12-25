4cf621c43ecd1d36df0339b65f0d80e8b3978e19
AhmedHAkeem <ahmedhakeem1599>
2025-12-25 21:06:42 +0200

    update pubspec

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class docApp extends StatelessWidget {
+  const docApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
