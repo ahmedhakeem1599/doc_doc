bb1154ad9c872cb1736ada0509e667c0f6faa7d8
AhmedHAkeem <ahmedhakeem1599>
2026-01-13 02:52:13 +0200

    edit light gray in colors

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -4,7 +4,7 @@ class AppColor {
   static const Color mainBlue = Color(0xFF247CFF);
   static const Color darkBlue = Color(0xFF242424);
   static const Color gray = Color(0xFF757575);
-  static const Color lightGray = Color(0xFFEDEDED);
+  static const Color lightGray = Color(0xFFC2C2C2);
   static const Color moreLightGray = Color(0xFFFDFDFF);
 
 
